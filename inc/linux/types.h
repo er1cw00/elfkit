cfcef08fafdb4951996c31b5ae4414f0fe8ec522
eric-ng <wadahana@qq.com>
2022-05-22 22:35:38 +0800

    feat: add android elf headers

---
+++ inc/linux/types.h
@@ -0,0 +1,13 @@
+#ifndef _UAPI_LINUX_TYPES_H
+#define _UAPI_LINUX_TYPES_H
+
+typedef signed char         __s8;
+typedef unsigned char       __u8;
+typedef short               __s16;
+typedef unsigned short      __u16;
+typedef int                 __s32;
+typedef unsigned int        __u32;
+typedef long long           __s64;
+typedef unsigned long long  __u64;
+
+#endif
