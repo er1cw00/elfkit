409d76a01d060f7851e26e9ab07f837724f3151c
eric.woo <wadahana@qq.com>
2022-07-09 17:38:54 +0800

    misc: rename func_array

--- src/elf_func_array.h
+++ src/elf_func_array.h
@@ -4,14 +4,16 @@
 
 class elf_func_array {
 public:
-    elf_func_array(addr_t array, size_t count, uint8_t elf_class) {
+    elf_func_array(addr_t array, size_t nums, uint8_t elf_class) {
         this->m_func_array = array;
-        this->m_func_count = count;
+        this->m_func_nums = nums;
         this->m_elf_class = elf_class;
     }
-    
+    size_t get_func_nums() {
+        return m_func_nums;
+    }
 protected:
     addr_t  m_func_array;
-    size_t  m_func_count;
+    size_t  m_func_nums;
     uint8_t m_elf_class;
 };
