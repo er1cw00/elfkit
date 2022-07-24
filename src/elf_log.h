03148add6644a3d925d8beeac031d23d0b09c36c
eric.woo <wadahana@qq.com>
2022-07-24 23:18:29 +0800

    misc: add log_trace

--- src/elf_log.h
+++ src/elf_log.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define log_trace(...)   do{ fprintf(stdout, __VA_ARGS__); } while(0)
 #define log_info(...)   do{ fprintf(stdout, __VA_ARGS__); } while(0)
 #define log_error(...)  do{ fprintf(stdout, __VA_ARGS__); } while(0)
 #define log_warn(...)   do{ fprintf(stdout, __VA_ARGS__); } while(0)
