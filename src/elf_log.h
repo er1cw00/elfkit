ce95f01e99a6ab8867653122e6c81f1dcc8db2f5
eric-ng <wadahana@qq.com>
2022-09-06 22:26:56 +0800

    feat: add elf_log

---
+++ src/elf_log.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define log_trace(...)   do{ fprintf(stdout, __VA_ARGS__); } while(0)
+#define log_info(...)    do{ fprintf(stdout, __VA_ARGS__); } while(0)
+#define log_error(...)   do{ fprintf(stdout, __VA_ARGS__); } while(0)
+#define log_warn(...)    do{ fprintf(stdout, __VA_ARGS__); } while(0)
+#define log_fatal(...)   do{ fprintf(stdout, __VA_ARGS__); } while(0)
+#define log_dbg(...)     do{ fprintf(stdout, __VA_ARGS__); } while(0)
