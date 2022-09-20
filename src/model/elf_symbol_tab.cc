51361413def26f976ab1998889f59c5c4e929d8a
eric-ng <wadahana@qq.com>
2022-09-20 23:39:04 +0800

    feat: 1.add symtab_size to elf_symbol_tab

--- src/model/elf_symbol_tab.cc
+++ src/model/elf_symbol_tab.cc
@@ -7,16 +7,16 @@
 bool elf_symbol_tab::get_symbol(const int i, elf_symbol* symbol) {
     if (symbol) {
         if (m_elf_class == ELFCLASS32) {
-            Elf32_Sym* sym = &((Elf32_Sym*)m_sym_tab)[i];
+            Elf32_Sym* sym = &((Elf32_Sym*)m_symtab)[i];
             elf_symbol_reset_with_sym32(symbol, sym);
         } else if (m_elf_class == ELFCLASS64) {
-            Elf64_Sym* sym = &((Elf64_Sym*)m_sym_tab)[i];
+            Elf64_Sym* sym = &((Elf64_Sym*)m_symtab)[i];
             elf_symbol_reset_with_sym64(symbol, sym);
         } else {
             return false;
         }
-        if (symbol && m_str_tab) {
-            const char * name = m_str_tab->get_string(symbol->st_name);
+        if (symbol && m_strtab) {
+            const char * name = m_strtab->get_string(symbol->st_name);
             symbol->sym_name = (char*)name;
             return true;
         }
--- src/model/elf_symbol_tab.h
+++ src/model/elf_symbol_tab.h
@@ -8,19 +8,19 @@ class elf_hash_tab;
 class elf_string_tab;
 class elf_symbol_tab {
 public:
-    elf_symbol_tab(Elf64_Sym* sym, elf_string_tab * strtab, elf_hash_tab * hashtab) {
+    elf_symbol_tab(Elf64_Sym* symtab, const size_t symtab_size, elf_string_tab * strtab) {
         log_trace("elf_symbol_tab ctor: %p\n", this);
-        this->m_sym_tab   = (addr_t)sym;
-        this->m_elf_class = ELFCLASS64;
-        this->m_str_tab   = strtab;
-        this->m_hash_tab  = hashtab;
+        this->m_symtab       = (addr_t)symtab;
+        this->m_symtab_size  = symtab_size;
+        this->m_strtab       = strtab;
+        this->m_elf_class    = ELFCLASS64;
     }
-    elf_symbol_tab(Elf32_Sym* sym, elf_string_tab * strtab, elf_hash_tab * hashtab) {
+    elf_symbol_tab(Elf32_Sym* symtab, const size_t symtab_size, elf_string_tab * strtab) {
         log_trace("elf_symbol_tab ctor: %p\n", this);
-        this->m_sym_tab   = (addr_t)sym;
-        this->m_elf_class = ELFCLASS32;
-        this->m_str_tab   = strtab;
-        this->m_hash_tab  = hashtab;
+        this->m_symtab       = (addr_t)symtab;
+        this->m_symtab_size  = symtab_size;
+        this->m_strtab       = strtab;
+        this->m_elf_class    = ELFCLASS32;   
     }
     ~elf_symbol_tab() {
         log_trace("elf_symbol_tab dtor: %p\n", this);
@@ -28,7 +28,7 @@ public:
     bool get_symbol(const int i, elf_symbol* symbol);
 protected:
     uint8_t         m_elf_class;
-    addr_t          m_sym_tab;
-    elf_string_tab* m_str_tab;
-    elf_hash_tab*   m_hash_tab;
+    addr_t          m_symtab;
+    size_t          m_symtab_size;
+    elf_string_tab* m_strtab;
 };
