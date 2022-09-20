5a666bde92e2ab74690e355efbd28d7981012009
eric-ng <wadahana@qq.com>
2022-09-20 23:40:12 +0800

    feat: add elfkit.h

---
+++ src/elfkit.h
@@ -0,0 +1,14 @@
+
+#pragma once
+#include <common/elf.h>
+#include <common/elf_common.h>
+#include <model/elf_type.h>
+#include <model/elf_func_array.h>
+#include <model/elf_hash_tab.h>
+#include <model/elf_sysv_hash_tab.h>
+#include <model/elf_gnu_hash_tab.h>
+#include <model/elf_reloc_tab.h>
+#include <model/elf_symbol_tab.h>
+#include <model/elf_string_tab.h>
+#include <reader/elf_reader.h>
+#include <image/elf_image.h>
