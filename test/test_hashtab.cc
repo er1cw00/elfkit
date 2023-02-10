dff6e46acd56165b312d58c4819c7403370f99a2
er1cw00 <wadahana@qq.com>
2023-02-10 23:33:36 +0800

    test: add unittest cmake

---
+++ test/test_hashtab.cc
@@ -0,0 +1,8 @@
+#include "common/elf.h"
+#include "model/elf_type.h"
+
+
+int main() {
+    printf("hashtab\n");
+    return 0;
+}
---
+++ test/test_types.cc
@@ -0,0 +1,8 @@
+#include "common/elf.h"
+#include "model/elf_type.h"
+
+
+int main() {
+    printf("xxxx\n"); exit(1);
+    return 0;
+}
